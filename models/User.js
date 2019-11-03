81ab1da6cec2d29924cdf70ae787772daeba353d
Elena Piaggio <elenapiaggio@gmail.com>
2019-11-03 23:29:09 +0100

    ADD user model

--- models/User.js
+++ models/User.js
@@ -6,6 +6,30 @@ const userSchema = new Schema(
   {
     username: { type: String, required: true, unique: true },
     hashedPassword: { type: String, required: true },
+    firstName: { type: String },
+    lastName: { type: String },
+    email: { type: String },
+    mobile: { type: Number },
+    studies: {
+      start: { type: Date },
+      end: { type: Date },
+      careerTitle: { type: String },
+      college: { type: String },
+      sector: { type: String },
+      comments: { type: String },
+    },
+    experience: {
+      company: { type: String },
+      job: { type: String },
+      sector: { type: String },
+      comments: { type: String },
+    },
+    languages: {
+      language: { type: String },
+      spokenLevel: { type: Number },
+      writtenLevel: { type: Number },
+      certificate: { type: String },
+    },
   },
   {
     timestamps: {
