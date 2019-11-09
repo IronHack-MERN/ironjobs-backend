93a691309997482fb981349d7a14f23410668d54
Elena Piaggio <elenapiaggio@gmail.com>
2019-11-09 14:27:35 +0100

    UPDATE configuration

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
-const cors = require('cors');
+const cors = require('cors')({ origin: true, credentials: true });
 require('dotenv').config();
 
 mongoose.set('useCreateIndex', true);
@@ -23,11 +22,13 @@ const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
 
 const app = express();
+app.set('trust proxy', true);
+app.use(cors);
+app.options('*', cors);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
@@ -39,8 +40,11 @@ app.use(
     secret: process.env.SECRET,
     resave: true,
     saveUninitialized: true,
+    name: 'ironjobs-ih', // configuracion del nombre de la cookie
     cookie: {
       maxAge: 24 * 60 * 60 * 1000,
+      sameSite: 'none', // esta es la linea importante
+      secure: process.env.NODE_ENV === 'production', // esta es la linea importante
     },
   }),
 );
