5864f95505dbffa2ba3608d147bdef0627a9273a
Elena Piaggio <elenapiaggio@gmail.com>
2019-11-13 12:31:56 +0100

    ADD job update route

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Jobs = require('../models/Job');
+const { checkIfLoggedIn } = require('../middlewares/index');
 
 const router = express.Router();
 
@@ -34,6 +35,44 @@ router.post('/', async (req, res, next) => {
     res.status(200).json(newJob);
   } catch (err) {
     next(`*** Alert -> You have an error: ${err}`);
+  }
+});
+
+router.put('/:id', checkIfLoggedIn, async (req, res, next) => {
+  const jobId = req.params.id;
+  const {
+    title, description, company, typePosition, specialty, salary, requeriments, isOffered, location,
+  } = req.body;
+
+  try {
+    const response = Jobs.findByIdAndUpdate(
+      jobId,
+      {
+        title,
+        description,
+        company,
+        typePosition,
+        specialty,
+        salary,
+        requeriments,
+        isOffered,
+        location,
+      },
+      { new: true },
+    )
+      .then((job) => {
+        res.json({ job });
+      })
+      .catch((error) => {
+        next(error);
+      });
+
+    res.status(200).json({
+      message: 'User updated successfully',
+      response,
+    });
+  } catch (err) {
+    next(`You have an error: ${err} in the job:  ${jobId}`);
   }
 });
 
